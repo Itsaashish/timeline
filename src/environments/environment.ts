bc503150c93ddb3bc94d3264e4bcc6406ada5129
aashish <itsaashish027@gmail.com>
2025-01-09 16:25:32 +0530

    rm:environemnt files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,1 @@
-export const environment = {
-    firebaseConfig:{
-        apiKey: "undefined",
-        authDomain: "undefined",
-        projectId: "undefined",
-        storageBucket: "undefined",
-        messagingSenderId: "undefined",
-        appId: "undefined",
-    }
-  }
-      
+   
